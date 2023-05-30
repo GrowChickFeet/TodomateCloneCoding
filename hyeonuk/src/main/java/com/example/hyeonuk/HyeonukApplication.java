a729fbebf98bfcdceb7a6b57c3970aabecea400a
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-05-30 16:29:38 +0900

    init : project init
    SpringBoot 프로젝트 init

---
+++ hyeonuk/src/main/java/com/example/hyeonuk/HyeonukApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyeonuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeonukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyeonukApplication.class, args);
+	}
+
+}
---
+++ hyeonuk/src/test/java/com/example/hyeonuk/HyeonukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyeonuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeonukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
